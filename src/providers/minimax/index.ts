fecc6f252318689c188ec1f9fd22fb040c37454f
Ahoo Wang <ahoowang@qq.com>
2026-05-30 08:39:36 +0800

    refactor: stop re-exporting internal protocol types from provider barrels

--- src/providers/minimax/index.ts
+++ src/providers/minimax/index.ts
@@ -1,5 +1,4 @@
 export * from "./client";
 export { createMiniMaxProviderEdge as createMiniMaxProvider } from "./client";
 export * from "./hooks";
-export * from "./protocol";
 export * from "./spec";
